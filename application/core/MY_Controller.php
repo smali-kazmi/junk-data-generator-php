dd85cd3b366874d6f7ed9a37608b76d0c887d21b
Mudaser Ali <smali.kazmi@hotmail.com>
2014-12-07 07:46:19 +0500

    connect with db using dns if db is not autoload

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -12,17 +12,41 @@
  * @author SMAK
  */
 class MY_Controller extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
         $this->init();
     }
-    
+
     private function init() {
-        echo getenv('cli');
-        echo "\n";
-        echo "im here";
-        echo "\n";
-        
+
+        $dbSettings = $this->getMappedCliArgs();
+
+        if (!isset($this->db)) {
+            $dsn = $dbSettings['driver']
+                    . '://'
+                    . $dbSettings['username']
+                    . ':'
+                    . $dbSettings['password']
+                    . '@'
+                    . $dbSettings['host']
+                    . '/'
+                    . $dbSettings['database'];
+
+            $this->load->database($dsn);
+        }
     }
-    
+
+    private function getMappedCliArgs() {
+        $options = getopt("d:u:p:h:c:");
+
+        return array(
+            'driver' => isset($options['l']) ? $options['l'] : 'mysql',
+            'username' => isset($options['u']) ? $options['u'] : 'root',
+            'password' => isset($options['p']) ? $options['p'] : '',
+            'host' => isset($options['h']) ? $options['h'] : 'localhost',
+            'database' => isset($options['d']) ? $options['d'] : ''
+        );
+    }
+
 }
